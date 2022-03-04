231f209b6cd5eca10d16572986c31adb66bf905c
rizalazky <rizalazky26@gmail.com>
2022-03-05 00:15:42 +0700

    update js

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <!-- <link href="assets/img/logo.png" rel="icon"> -->
+  <link href="assets/img/bg1.JPG" rel="icon">
   <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
